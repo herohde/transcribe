cd249b51080d0a05fc88e8f29c63f66430aaa517
Henning Korsholm Rohde <dane_127@hotmail.com>
2017-06-11 18:19:18 -0700

    Add basic transcribe functionality

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # transcribe
+
+Transcribe is a tool for transcribing audio files using Google Speech API. It
+is intended for bulk processing of large (> 1 min) audio files and automates
+GCS upload (and removal).
+
+# How to use
+
+First, ensure you have Google Speech API enabled in your project as described here:
+https://cloud.google.com/speech/docs/getting-started.
+
+$ transcribe [options] file [...]
---
+++ cmd/transcribe/main.go
@@ -0,0 +1,184 @@
+// transcribe is a tool for transcribing audio files using Google Speech API. It
+// is intended for bulk processing of large (> 1 min) audio files and automates
+// GCS upload (and removal).
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"cloud.google.com/go/speech/apiv1"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/storage/v1"
+
+	speechpb "google.golang.org/genproto/googleapis/cloud/speech/v1"
+	"path"
+	"sync"
+)
+
+var (
+	project = flag.String("project", "", "GCP project to use. The project must have the Speech API enabled.")
+	output  = flag.String("out", ".", "Directory to place output text files.")
+	bucket  = flag.String("bucket", "", "Temporary GCS bucket to hold the audio files. If not provided, a new transient bucket will be created.")
+)
+
+func init() {
+	flag.Usage = func() {
+		fmt.Fprint(os.Stderr, `
+usage: transcribe [options] file [...]
+
+Transcribe transcribes audio files using Google Speech API. It is intended
+for bulk processing of large (> 1 min) audio files and automates GCS upload
+(and removal). Supported formats: flac.
+Options:
+`)
+		flag.PrintDefaults()
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	files := flag.Args()
+	if len(files) == 0 {
+		fmt.Fprintln(os.Stderr, "transcribe: no files provided.")
+		flag.Usage()
+		os.Exit(1)
+	}
+	if *project == "" {
+		fmt.Fprintln(os.Stderr, "transcribe: no project provided.")
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	// (2) Create tmp location, if needed.
+
+	cl, err := newStorageClient(context.Background())
+	if err != nil {
+		log.Fatalf("Failed to create GCS client: %v", err)
+	}
+
+	if *bucket == "" {
+		*bucket = fmt.Sprintf("transcribe-%v", time.Now().UnixNano())
+
+		if _, err := cl.Buckets.Insert(*project, &storage.Bucket{Name: *bucket}).Do(); err != nil {
+			log.Fatalf("Failed to create tmp bucket %v: %v", *bucket, err)
+		}
+		defer func() {
+			if err := cl.Buckets.Delete(*bucket).Do(); err != nil {
+				log.Printf("Failed to delete tmp bucket %v: %v", *bucket, err)
+			}
+		}()
+	}
+
+	scl, err := speech.NewClient(context.Background())
+	if err != nil {
+		log.Fatalf("Failed to create speech client: %v", err)
+	}
+
+	log.Printf("Transcribing %v files in parallel", len(files))
+
+	// (3) Upload and transcribe the files in parallel
+
+	var wg sync.WaitGroup
+	for _, name := range files {
+		wg.Add(1)
+		go func(name string) {
+			defer wg.Done()
+
+			if !strings.HasSuffix(strings.ToLower(name), ".flac") {
+				log.Printf("File %v is not a supported format. Ignoring.", name)
+				return
+			}
+
+			log.Printf("Transcribing %v ...", filepath.Base(name))
+
+			// (a) Upload
+
+			obj := path.Join("tmp/audio", strings.ToLower(filepath.Base(name)))
+			fd, err := os.Open(name)
+			if err != nil {
+				log.Printf("Failed to read %v: %v", name, err)
+				return
+			}
+			defer fd.Close()
+
+			if _, err := cl.Objects.Insert(*bucket, &storage.Object{Name: obj}).Media(fd).Do(); err != nil {
+				log.Printf("Failed to upload %v: %v", name, err)
+				return
+			}
+			defer cl.Objects.Delete(*bucket, obj).Do()
+
+			// (b) Transcribe
+
+			req := &speechpb.LongRunningRecognizeRequest{
+				Config: &speechpb.RecognitionConfig{
+					Encoding:        speechpb.RecognitionConfig_FLAC,
+					SampleRateHertz: 44100,
+					LanguageCode:    "en-US",
+				},
+				Audio: &speechpb.RecognitionAudio{
+					AudioSource: &speechpb.RecognitionAudio_Uri{Uri: fmt.Sprintf("gs://%v/%v", *bucket, obj)},
+				},
+			}
+
+			op, err := scl.LongRunningRecognize(context.Background(), req)
+			if err != nil {
+				log.Printf("Failed to transcribe %v: %v", name, err)
+				return
+			}
+			resp, err := op.Wait(context.Background())
+			if err != nil {
+				log.Printf("Failed to transcribe %v: %v", name, err)
+				return
+			}
+
+			// TODO(herohde) 6/11/2017: Add simple post-processing.
+
+			var phrases []string
+			for _, result := range resp.Results {
+				for _, alt := range result.Alternatives {
+					// Add text, if low confidence?
+					phrases = append(phrases, alt.Transcript)
+				}
+			}
+			data := strings.Join(phrases, " ")
+
+			// (c) Write output
+
+			base := filepath.Join(*output, filepath.Base(name))
+
+			if err := ioutil.WriteFile(base+".raw.txt", []byte(data), 0644); err != nil {
+				log.Printf("Failed to write raw output: %v", err)
+			}
+
+			// data = strings.Replace(data, "paragraph", "\n", -1)
+			// data = strings.Replace(data, "Paragraph", "\n", -1)
+			data = strings.Replace(data, "  ", " ", -1)
+
+			if err := ioutil.WriteFile(base+".txt", []byte(data), 0644); err != nil {
+				log.Printf("Failed to write clean output: %v", err)
+			}
+
+			log.Printf("Transcribed %v", filepath.Base(name))
+		}(name)
+	}
+	wg.Wait()
+
+	log.Print("Done")
+}
+
+func newStorageClient(ctx context.Context) (*storage.Service, error) {
+	httpClient, err := google.DefaultClient(context.Background(), storage.CloudPlatformScope)
+	if err != nil {
+		return nil, err
+	}
+	return storage.New(httpClient)
+}
